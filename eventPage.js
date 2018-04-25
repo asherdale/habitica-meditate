a71242a5dd6a9b0ac4179cebf167a80eee507d9f
Asher Dale <asher@dales.org>
2018-04-25 21:12:10 +0300

    Created method to get accurate CSV export from Insight Timer

--- eventPage.js
+++ eventPage.js
@@ -1,37 +1,42 @@
-const HABITICA_EMAIL = 'asher@dales.org';
-const HABITICA_PASSWORD = 'y5^%1vl5&U@ns';
-
-const INSIGHT_EMAIL = 'asor1999@gmail.com';
-const INSIGHT_PASSWORD = 'asherd';
+const INSIGHT_LOGIN_URL = 'https://insighttimer.com/user_session';
+const INSIGHT_CSV_URL = 'https://insighttimer.com/sessions/export';
+const HABITICA_LOGIN_URL = 'https://habitica.com/api/v3/user/auth/local/login';
+const HABITICA_PREFS_URL = 'https://habitica.com/api/v3/user?userFields=preferences';
 
 checkMeditationData = async (alarm) => {
   await $.ajax({
-    url: 'https://insighttimer.com/user_session',
+    url: INSIGHT_LOGIN_URL,
     type: 'POST',
     data: {
       'user_session[email]': INSIGHT_EMAIL,
       'user_session[password]': INSIGHT_PASSWORD,
     }
   });
+  
   const meditationData = await getMeditationData();
-  for (let i = 0; i < 30; i++) {
-    const res = await getMeditationData();
-    console.log(res[2][0]);
-  }
-  return;
   const customDayStart = await getHabiticaCustomDayStart();
-  const minsMeditatedToday = await getMinsMeditatedToday(meditationData, customDayStart);
-  console.log('Minutes meditated today: ', minsMeditatedToday);
+  const minsToday = await getMinsMeditatedToday(meditationData, customDayStart);
+  console.log('Minutes meditated today: ', minsToday);
 }
 
 getMeditationData = async () => {
-  const firstRes = await $.ajax('https://insighttimer.com/sessions/export');
-  await new Promise(resolve => setTimeout(resolve, 3000));
-  const secondRes = await $.ajax('https://insighttimer.com/sessions/export');
-  // const thirdRes = await $.ajax('https://insighttimer.com/sessions/export');
-  return firstRes[2][0] === secondRes[2][0] ? // && firstRes[2][0] === thirdRes[2][0] ?
-         $.csv.toArrays(firstRes) :
-         await getMeditationData();
+  const numGrabs = 7;
+  const vals = await Promise.all(
+    Array.from(
+      {length: numGrabs},
+      i => $.ajax(INSIGHT_CSV_URL)
+    )
+  );
+  const csvs = vals.map(val => $.csv.toArrays(val));
+  const times = {};
+  csvs.forEach((csv) => {
+    const time = csv[2][0];
+    times[time] = times[time] ? times[time] + 1 : 1;
+  });
+  const accurateTime = Object.keys(times).find(t => times[t] > numGrabs / 2);
+  return accurateTime ? 
+         csvs.find(csv => csv[2][0] === accurateTime) :
+         getMeditationData();
 }
 
 getMinsMeditatedToday = (meditationData, customDayStart) => {
@@ -39,14 +44,14 @@ getMinsMeditatedToday = (meditationData, customDayStart) => {
   let i = 2;
   const habiticaDayStart = moment().startOf('day').add(customDayStart, 'h');
   while (meditationData[i]) {
-    const offsetSessionDate = moment(meditationData[i][0], 'MM-DD-YYYY HH:mm:ss');
-    const correctSessionDate = offsetSessionDate.clone().add(offsetSessionDate.utcOffset(), 'm');
+    const offsetTime = moment(meditationData[i][0], 'MM-DD-YYYY HH:mm:ss');
+    const correctTime = offsetTime.clone().add(offsetTime.utcOffset(), 'm');
     const sessionMins = parseInt(meditationData[i][1]);
     i += 1;
 
-    console.log(correctSessionDate.format());
-    console.log(correctSessionDate.diff(habiticaDayStart, 'h'));
-    if (!correctSessionDate.isSame(moment(), 'day')) {
+    console.log(correctTime.format());
+    console.log(correctTime.diff(habiticaDayStart, 'h'));
+    if (!correctTime.isSame(moment(), 'day')) {
       break;
     }
 
@@ -57,7 +62,7 @@ getMinsMeditatedToday = (meditationData, customDayStart) => {
 
 getHabiticaCustomDayStart = async () => {
   const user = await $.ajax({
-    url: 'https://habitica.com/api/v3/user/auth/local/login',
+    url: HABITICA_LOGIN_URL,
     type: 'POST',
     data: {
       'username': HABITICA_EMAIL,
@@ -65,7 +70,7 @@ getHabiticaCustomDayStart = async () => {
     }
   });
   const userData = await $.ajax({
-    url: 'https://habitica.com/api/v3/user?userFields=preferences',
+    url: HABITICA_PREFS_URL,
     beforeSend: (xhr) => {
       xhr.setRequestHeader('x-api-user', user.data.id);
       xhr.setRequestHeader('x-api-key',  user.data.apiToken);

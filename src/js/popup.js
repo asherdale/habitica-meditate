fff45a03c013c845b842c5a4f57279fbb054d2f5
Asher Dale <asher@dales.org>
2018-04-28 16:44:30 +0300

    Save login info to chrome.storage for auto login

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,55 +1,82 @@
+const LOGIN_INFO = {
+  hUser: 'habiticaUserId',
+  hToken: 'habiticaApiToken',
+  iEmail: 'insightEmail',
+  iPass: 'insightPassword'
+};
+
 const INSIGHT_LOGIN_CONFIG = {
   header: 'Step 2: Login to Insight Timer',
   userPlaceholder: 'Email',
   passPlaceholder: 'Password'
 };
 
-let HABITICA_LOGIN = true;
+let isHabiticaLogin = true;
 
 $(document).ready(() => main());
 
 main = async () => {
   $('#login-button').click(login);
+
+  const savedLoginInfo = await readData(Object.values(LOGIN_INFO));
+  if (!savedLoginInfo.habiticaUserId || !savedLoginInfo.habiticaApiToken) {
+    return;
+  }
+  const habiticaLoginAttempt = await clientHabiticaLogin(savedLoginInfo.habiticaUserId, savedLoginInfo.habiticaApiToken, true);
+  if (habiticaLoginAttempt) {
+    isHabiticaLogin = false;
+    if (!savedLoginInfo.insightEmail || !savedLoginInfo.insightPassword) {
+      return;
+    }
+    await clientInsightLogin(savedLoginInfo.insightEmail, savedLoginInfo.insightPassword, true);
+  }
 }
 
 login = () => {
   const loginInput = $('input').map((i, el) => $(el).val()).get();
-  return HABITICA_LOGIN ? clientHabiticaLogin(loginInput[0], loginInput[1])
-                        : clientInsightLogin(loginInput[0], loginInput[1]);
+  return isHabiticaLogin ? clientHabiticaLogin(loginInput[0], loginInput[1], false)
+                         : clientInsightLogin(loginInput[0], loginInput[1], false);
 }
 
-clientHabiticaLogin = async (userId, apiToken) => {
+clientHabiticaLogin = async (userId, apiToken, isAutoLogin) => {
   try {
-    const userData = await getHabiticaCustomStart(HABITICA_USER_ID, HABITICA_API_TOKEN);
-    await saveData({habiticaUserId: userId, habiticaApiToken: apiToken});
+    const userData = await getHabiticaCustomStart(userId, apiToken);
+    if (!isAutoLogin) {
+      await saveData({[LOGIN_INFO.hUser]: userId, [LOGIN_INFO.hToken]: apiToken});
+    }
     transferToInsightLogin();
+    return true;
   } catch (e) {
-    // TODO: show error for incorrect login info
-    dump(`Habitica login error: ${e}`);
+    dump('Habitica login error');
   }
 }
 
 transferToInsightLogin = () => {
-  HABITICA_LOGIN = false;
+  isHabiticaLogin = false;
   $('#login-header').text(INSIGHT_LOGIN_CONFIG.header);
+  
   const resetToPlaceholder = ($el, placeholder) => {
     $el.val('');
     $el.attr('placeholder', placeholder);
   }
+
   resetToPlaceholder($('#input-user'), INSIGHT_LOGIN_CONFIG.userPlaceholder);
   resetToPlaceholder($('#input-pass'), INSIGHT_LOGIN_CONFIG.passPlaceholder);
 }
 
-clientInsightLogin = async (email, password) => {
-  const loginResult = await insightLogin(INSIGHT_EMAIL, INSIGHT_PASSWORD);
-  dump(loginResult);
+clientInsightLogin = async (email, password, isAutoLogin) => {
+  const loginResult = await insightLogin(email, password);
   if (loginResult.toLowerCase().includes('sign')) {
-    // TODO: show insight error
-    return dump('Insight login error');
+    dump('Insight login error');
+    return false;
   }
-  dump('Success!');
-  await saveData({insightEmail: email, insightPassword: password});
+
+  if (!isAutoLogin) {
+    await saveData({[LOGIN_INFO.iEmail]: email, [LOGIN_INFO.iPass]: password});
+  }
+
   showContainer('create-container');
+  return true;
 }
 
 showContainer = (containerId) => {
@@ -63,32 +90,35 @@ dump = (input) => {
 
 saveData = async (obj) => {
   await chromep.storage.sync.set(obj);
-  dump(`${JSON.stringify(obj)} saved`);
 }
 
 readData = async (keys) => {
   return chromep.storage.sync.get(keys);
 }
 
-// createDaily = async () => {
-//   return $.ajax({
-//     url: 'https://habitica.com/api/v3/tasks/user',
-//     type: 'POST',
-//     data: {'text': `Meditate for ${medLength} minutes`, 'type': 'daily',},
-//     beforeSend: (xhr) => {
-//       xhr.setRequestHeader('x-api-user', items.userID);
-//       xhr.setRequestHeader('x-api-key',  items.apiToken);
-//     },
-//   });
-// }
+clearData = async () => {
+  await chromep.storage.sync.clear();
+}
 
-// scoreDaily = async () => {
-//   return $.ajax({
-//     url: `https://habitica.com/api/v3/tasks/${items.taskID}/score/up`,
-//     type: 'POST',
-//     beforeSend: (xhr) => {
-//       xhr.setRequestHeader('x-api-user', items.userID);
-//       xhr.setRequestHeader('x-api-key',  items.apiToken);
-//     },
-//   });
-// }
+createDaily = async (userId, apiToken) => {
+  return $.ajax({
+    url: 'https://habitica.com/api/v3/tasks/user',
+    type: 'POST',
+    data: {'text': `Meditate for ${medLength} minutes`, 'type': 'daily',},
+    beforeSend: (xhr) => {
+      xhr.setRequestHeader('x-api-user', userId);
+      xhr.setRequestHeader('x-api-key',  apiToken);
+    },
+  });
+}
+
+scoreDaily = async (userId, apiToken) => {
+  return $.ajax({
+    url: `https://habitica.com/api/v3/tasks/${items.taskID}/score/up`,
+    type: 'POST',
+    beforeSend: (xhr) => {
+      xhr.setRequestHeader('x-api-user', userId);
+      xhr.setRequestHeader('x-api-key',  apiToken);
+    },
+  });
+}
